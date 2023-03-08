be1ddc8251b20a57d3501ec5c3d6c48958b01d07
Dawid Zieba <d.zieba2@gmail.com>
2023-03-08 10:26:12 +0100

    added theme for colors

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+    color: {
+        teal: "#008080",
+        silver: "#ccc",
+        bondiBlue: "#00B3B3",
+    },
+    breakpoint: {
+        mobileMax:767,
+    },
+    };
