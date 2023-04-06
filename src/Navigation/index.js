62438e69fa256173b46da109e43b366644f19813
Dawid Zieba <d.zieba2@gmail.com>
2023-04-06 20:36:36 +0200

    Navigation component created

---
+++ src/Navigation/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { toAuthor, toTasks } from "../routes";
+import { List, Item, StyledNavLink, Nav } from "./styled";
+
+const Navigation = () => (
+    <Nav>
+      <List>
+        <Item>
+          <StyledNavLink to={toTasks()}>
+            Zadania
+            </StyledNavLink>
+        </Item>
+        <Item>
+          <StyledNavLink to={toAuthor()}>
+            O autorze
+            </StyledNavLink>
+        </Item>
+      </List>
+      </Nav>
+      );
+
+      export default Navigation
---
+++ src/Navigation/styled.js
@@ -0,0 +1,35 @@
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+export const Nav = styled.nav`
+    background-color: ${({ theme }) => theme.color.teal};
+`;
+
+export const List = styled.ul`
+    list-style-type: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 10px;
+    margin: 0;
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+      flex-direction: column;
+      align-items: center;
+    }
+`;
+
+export const Item = styled.li`
+    padding: 10px 20px;
+    font-size: 18px;
+`;
+
+export const StyledNavLink = styled(NavLink)`
+    text-decoration: none;
+    color: ${({ theme }) => theme.color.white};
+    &:active {
+      font-weight: bold;
+    }
+    &:hover {
+      border-bottom: 1px solid;
+    }
+`;
